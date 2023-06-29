105d322d8852cb72e2065a0b29110fcac9037dd1
HetDesai8 <desaihet8@gmail.com>
2023-06-29 11:29:02 +0530

    initial

---
+++ user-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y nginx
+sudo service nginx start
+
+sudo sh -c 'echo "<html><body><h1>Hello World -dev</h1></body></html>" > /usr/share/nginx/html/index.html'
