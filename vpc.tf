2ed92422e54d289e63572379b1c946ad3a4205c0
HetDesai8 <desaihet8@gmail.com>
2023-06-30 14:28:15 +0530

    vpc

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "prod-vpc"
+  name = "Het-prod-vpc"
   cidr = "11.0.0.0/16"
 
   azs             = ["us-east-1a", "us-east-1b"]
